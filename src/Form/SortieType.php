d6a33329721fe3550e868ba70a0839c26af3fbca
Melvin AIME <melvin.aime2022@campus-eni.fr>
2023-10-11 14:20:38 +0200

    start making sorties creation

---
+++ src/Form/SortieType.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Lieu;
+use App\Entity\Sortie;
+use App\Entity\Villes;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'label' => "Nom de la sortie"
+            ])
+            ->add('dateDebut', DateTimeType::class, [
+                'label' => 'Date de debut de la sortie',
+                'widget' => 'single_text',
+            ])
+            ->add('dateRetour', DateTimeType::class, [
+                'mapped' => false,
+                'label' => 'Date de retour',
+                'widget' => 'single_text'
+            ])
+            ->add('dateLimiteInscription', DateType::class, [
+                'label' => "Date de fin d'inscription",
+                'widget' => 'single_text'
+            ])
+            ->add('nbInscriptionMax', IntegerType::class, [
+                'label' => "Nombre de places",
+            ])
+            ->add('infosSortie', TextType::class, [
+                'label' => "Description et infos",
+            ])
+            ->add('lieux', EntityType::class, [
+                'label' => "Lieu",
+                'class' => Lieu::class,
+                'choice_label' => 'nom',
+                'multiple' => false,
+                'expanded' => false,
+                'by_reference' => false,
+            ])
+            ->add('ville', EntityType::class, [
+                'label' => "Ville",
+                'mapped' => false,
+                'class' => Villes::class,
+                'choice_label' => 'nom'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
