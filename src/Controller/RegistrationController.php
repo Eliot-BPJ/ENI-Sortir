3daca07345886454f38c3ac03a308a45657ac1f3
vmarsac2022 <victor.marsac2279@gmail.com>
2023-10-12 10:58:20 +0200

    fix registercontroller

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -75,12 +75,12 @@ class RegistrationController extends AbstractController
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
             // do anything else you need here, like send an email
-            if($this->getUser()->getRoles()[0] === 'ROLE_ADMIN') {
+            if($this->getUser() && $this->getUser()->getRoles()[0] === 'ROLE_ADMIN') {
                 return $this->redirectToRoute('app_admin_utilisateur_lister');
             }
             return $this->redirectToRoute('app_login');
         }
-        if($this->getUser()->getRoles()[0] === 'ROLE_ADMIN') {
+        if($this->getUser() && $this->getUser()->getRoles()[0] === 'ROLE_ADMIN') {
             return $this->render('admin/utilisateur/adminRegister.html.twig', [
                 'registrationForm' => $form->createView(),
             ]);
