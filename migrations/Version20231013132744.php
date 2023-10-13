8f74d72c00f56033b3d81a0076585154fea918df
vmarsac2022 <victor.marsac2279@gmail.com>
2023-10-13 16:11:29 +0200

    Reset password

---
+++ migrations/Version20231013132744.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231013132744 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE password_token (id INT AUTO_INCREMENT NOT NULL, utilisateur_id INT NOT NULL, token VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_BEAB6C24FB88E14F (utilisateur_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE password_token ADD CONSTRAINT FK_BEAB6C24FB88E14F FOREIGN KEY (utilisateur_id) REFERENCES utilisateur (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE password_token DROP FOREIGN KEY FK_BEAB6C24FB88E14F');
+        $this->addSql('DROP TABLE password_token');
+    }
+}
